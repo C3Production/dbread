3e8bdaef396c04e2a70aee4c9d1549c54f376fb8
Wong Chui Ching <wongchuiching98@gmail.com>
2021-06-18 09:01:07 +0800

    Initialize

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,21 @@ export default function Home() {
   return (
     <div className='container'>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>D' Bread</title>
+        <link rel='icon' type='image/png' href='/logo.png' />
       </Head>
 
       <main>
-        <Header title="Welcome to D' Bread!" />
+        <Header title="Welcome to D' Little Bread Cafe!" />
         <p className='description'>
-          This page is under construction now. Please visit our{' '}
+          This page is under construction now.
+          <br /> Please visit our{' '}
           <a href='https://www.facebook.com/D-Bread-111968957785306'>
             Facebook Page
           </a>{' '}
           for more information.
+          <br />
+          We're making lots of improvements and will be back soon.
         </p>
       </main>
 
