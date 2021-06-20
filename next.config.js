50ea2b2dabdb053feedcb1f2f09835222ca92a84
Wong Chui Ching <wongchuiching98@gmail.com>
2021-06-20 09:14:19 +0800

    Update Privacy Policy and Terms of Service

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 const path = require('path');
-const withSass = require('@zeit/next-sass');
 const nextTranslate = require('next-translate');
 
-module.exports = withSass({
-  /* bydefault config  option Read For More Optios
-here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
-  cssModules: true,
-});
-
 module.exports = {
-  /* Add Your Scss File Folder Path Here */
+  /* Add Scss File Folder Path Here */
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
